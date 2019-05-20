29c393b6e803812b4e051592074529253267b580
Deepankar Sahu <36103319+dany536@users.noreply.github.com>
2019-05-20 14:40:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -23,8 +23,8 @@ try {
     $mail->isSMTP();                                            // Set mailer to use SMTP
     $mail->Host       = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'techpvec1@gmail.com';                  // SMTP username
-    $mail->Password   = 'wqzlvttbvlikwuph';                     // SMTP password
+    $mail->Username   = 'Your-email-id';                  // SMTP username
+    $mail->Password   = 'your-password';                     // SMTP password
     $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
     $mail->Port       = 587;                                    // TCP port to connect to
     
